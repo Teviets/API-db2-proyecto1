81c73f2c90b9581ef21746077e1d1107b20888bd
Sebastian Estrada <est21405@uvg.edu.gt>
2024-02-01 16:33:38 -0600

    Inicio de proyecto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const config = require('./config');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+}));
+
+// app.use(require('./routes/index'))
+
+app.listen(config.port);
+
+console.log('Server on Port: ', config.port);
