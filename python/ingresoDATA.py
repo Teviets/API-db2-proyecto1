b118814ca69e9c0c4d5b4392f69bf2bd2b69dd44
Sebastian Estrada <est21405@uvg.edu.gt>
2024-02-20 20:52:52 -0600

    Publicacion de datos a mongo

--- python/ingresoDATA.py
+++ python/ingresoDATA.py
@@ -1,44 +1,85 @@
 import fake as f
 import random as r
 
+from pymongo import MongoClient
+
+dbURL = "mongodb+srv://devsebasest37:040902@proyectodb2.cqgnmx4.mongodb.net/"
+
+client = MongoClient(dbURL)
+
+usuarios = []
+restaurantes = []
+platillos = []
+reservas = []
+calificaciones = []
+
 # obtener nombre y apellido, correos electronicos, contraseñas, rol ("admin", "user", "owner") con fake 50,000  registros
-def get_data():
-    data = []
+# 25 k registros
+def get_users():
+    
+    # agregar restaurantes solo si son owners
     roles = ["admin", "user", "owner"]
-    for _ in range(50000):
-        data.append({
+    rol = r.randint(0,2)
+    ids = []
+    for _ in range(25000):
+
+        while True:
+            id = r.randint(0, 25000)
+            if id not in ids:
+                ids.append(id)
+                break
+
+        usuarios.append({
+            "id": id,
             "nombre": f.FAKER.first_name(),
             "apellido": f.FAKER.last_name(),
             "correo": f.FAKER.email(),
             "contraseña": f.FAKER.word(),
-            "rol": roles[r.randint(0,2)]#f.FAKER.random_element(elements=("admin", "user", "owner"))
+            "rol": rol,
+            "restaurantes": [restaurantes[r.randint(0, 4999)] for _ in range(r.randint(1, 5))] if (rol == 0 or rol == 1)  else []
         })
-    return data
 
 # obtener nombre (de platillo), precio (de 30 a 200), descripcion (de 10 a 30 palabras), id de restaurante (de 0 a 50,000)
+# 8 k registros
 def get_platillos():
-    data = []
-    for i in range(50000):
-        data.append({
-            "id": r.randint(0, 50000),
+    ids = []
+    for i in range(8000):
+
+        while True:
+            id = r.randint(0, 24999)
+            if id not in ids:
+                ids.append(id)
+                break
+
+        platillos.append({
+            "id": id,
             "nombre": f.FAKER.word()+f.FAKER.word(),
             "precio": r.uniform(30, 200),
-            "descripcion": f.FAKER.words(nb=30),
+            "descripcion": f.FAKER.sentence(nb_words=30),
             "id_restaurante": r.randint(0, 50000)
         })
-    return data
 
 # obtener nombre (de restaurante) ubicacion (latitud y longitud de guatemal), id (de 0 a 50,000), menu (lista de diccionario con nombre y precio de 4 a 10)
+# 5 k registros
 def get_restaurantes():
-    data = []
-    platillos = get_platillos()
     num_platillos = len(platillos)
+    ids = []
 
-    for i in range(50000):
-        vals = [platillos[r.randint(0, num_platillos - 1)] for _ in range(r.randint(4, 10))]
-        data.append({
-            "id": r.randint(0, 50000),
+    for i in range(5000):
+        vals = [platillos[r.randint(0, num_platillos - 1)] for _ in range(r.randint(4, 12))]
+
+        while True:
+            id = r.randint(0, 24999)
+            if id not in ids:
+                ids.append(id)
+                break
+
+        restaurantes.append({
+            "id": id,
             "nombre": f.FAKER.word() + f.FAKER.word(),
+            "img": f.FAKER.image_url(),
+            "descripcion": f.FAKER.sentence(nb_words=30),
+            "rating": r.uniform(0.5, 5),
             "ubicacion": {
                 "latitud": r.uniform(14.5, 15.5),
                 "longitud": r.uniform(90.5, 91.5)
@@ -47,45 +88,69 @@ def get_restaurantes():
                 {"nombre": vals[y]["nombre"], "precio": vals[y]["precio"]} for y in range(len(vals))
             ]
         })
-    return data
 
 
 # obtener id de restaurante, id de usuario, fecha (de febrero 2024 a febrero 2025), cantidad de personas (de 1 a 20), comentarios de reserva (de 10 a 30 palabras)
+# 10 k registros
 def get_reservas():
-    data = []
-    for i in range(50000):
-        data.append({
-            "id_restaurante": r.randint(0, 50000),
-            "id_usuario": r.randint(0, 50000),
-            "fecha": f.FAKER.date(start_date="-0d", end_date="+365d"),
+    ids = []
+    for i in range(10000):
+
+        date = str(f.FAKER.date_time(start_date="-0d", end_date="+365d"))
+        while True:
+            id = r.randint(0, 24999)
+            if id not in ids:
+                ids.append(id)
+                break
+
+        reservas.append({
+            "id": id,
+            "id_restaurante": r.randint(0, 4999),
+            "id_usuario": r.randint(0, 24999),
+            "fecha": date,
             "cantidad_personas": r.randint(1, 20),
-            "comentarios": f.FAKER.words(nb=30)
+            "comentarios": f.FAKER.sentence(nb_words=30)
         })
-    return data
 
 # obtener id de restaurante, clienteid, fecha (de febrero 2024 a febrero 2025), puntuacion de 0 a 5, comentarios de calificacion (de 10 a 30 palabras)
+# 2 k registros
 def get_calificaciones():
-    data = []
-    for i in range(50000):
-        data.append({
-            "id_restaurante": r.randint(0, 50000),
-            "clienteid": r.randint(0, 50000),
-            "fecha": f.FAKER.date(start_date="-0d", end_date="+365d"),
-            "puntuacion": r.randint(0, 5),
-            "comentarios": f.FAKER.words(nb=30)
+    ids = []
+    for i in range(2000):
+        while True:
+            id = r.randint(0, 2999)
+            if id not in ids:
+                ids.append(id)
+                break
+
+        calificaciones.append({
+            "id": id,
+            "id_restaurante": r.randint(0, 4999),
+            "clienteid": r.randint(0, 24999),
+            "fecha": str(f.FAKER.date(start_date="-0d", end_date="+365d")),
+            "puntuacion": r.uniform(0, 5),
+            "comentarios": f.FAKER.sentence(nb_words=30)
         })
-    return data
 
 
+get_platillos()
+get_restaurantes()
+get_users()
+get_reservas()
+get_calificaciones()
 
-data = get_data()
-platillos = get_platillos()
-restaurantes = get_restaurantes()
-reservas = get_reservas()
-calificaciones = get_calificaciones()
 
-print(data)
-print(platillos)
-print(restaurantes)
-print(reservas)
-print(calificaciones)
+# insertar datos en la base de datos
+db = client["TasteTrail"]
+
+db["User"].insert_many(usuarios)
+db["Plate"].insert_many(platillos)
+db["Restaurant"].insert_many(restaurantes)
+db["Reservation"].insert_many(reservas)
+db["Ratings"].insert_many(calificaciones)
+
+print(usuarios)
+#print(platillos)
+#print(restaurantes)
+#print(reservas)
+#print(calificaciones)
